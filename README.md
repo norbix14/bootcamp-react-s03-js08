0bb568aecbb70b0afbdacb450d86efc7cebb62f9
Norberto <norgit@protonmail.com>
2021-11-12 09:45:57 -0300

    [create: dirs|files] make exercise js08

---
+++ README.md
@@ -0,0 +1,51 @@
+# Ejercicios S-03 JS08
+
+### Ejercicio N°1:
+`arr = [1,3,4,2,6,7,9,1, 8]`
+Crear una función que indique el total de elementos impares de un array
+
+```
+output => 5
+```
+
+### Ejercicio N°2:
+`str = "hola mundo desde javascript"`
+pasar un string en array, indicar los espacios en blanco por "-"
+
+```
+output => ["hola","-", "mundo", "-", "desde", "-", "javascript"]
+
+```
+
+### Ejercicio N°3:
+Crear una variable que contenga el siguiente array:
+`[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]`
+Definir una variable `filtered` que muestre los valores de los los numeros que son pares utilizando el método `.filter()`
+
+
+### Ejercicio N°4
+Crear una variable que filtre los animales que no sean 'cat' usar filter
+`const pets = ['cat', 'dog', 'elephant','hawk','snake','cat']`
+
+
+### Ejercicio N°5
+Crear una variable que muestre solo los nombres de los productos del siguiente array.
+hacerlo con `.map()`
+
+```
+const productos = [
+    {id:2, nombre: 'Remera muy fachera', precio:250},
+    {id:5, nombre: 'Pantalon muy fachero', precio:400},
+    {id:6, nombre: 'Camperon invierno', precio:699},
+    {id:34, nombre: 'Guantes negros', precio:199},
+];
+```
+
+### Ejercicio N°6
+Usando el reduce retornar un solo array.
+pista: `.concat()`
+
+```
+const data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+output => [1,2,3,4,5,6,7,8,9]
+```
---
+++ js/main.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const toTop = document.querySelector('#toTop')
+
+	window.addEventListener('scroll', controlScroll)
+
+	function controlScroll() {
+		const st = document.body.scrollTop || document.documentElement.scrollTop
+		const display = toTop.style.display
+		const cl = toTop.classList
+		if (st > 300) {
+			toTop.style.display = 'block'
+			cl.remove('hide')
+			cl.add('show')
+			setTimeout(() => {cl.add('rotate')}, 2100)
+	  } else {
+			if (display === 'block') {
+				cl.remove('show')
+				cl.add('hide')
+				setTimeout(() => {
+					cl.remove('rotate')
+					toTop.style.display = 'none'
+				}, 2000)
+			}
+	  }
+	}
+
+	toTop.addEventListener('click', () => {
+		document.body.scrollTop = 0
+		document.documentElement.scrollTop = 0
+	})
+
+	const ej01 = (arr = [1,3,4,2,6,7,9,1,8]) => arr.filter(el => el % 2 === 1).length
+	const ej02 = (str = 'hola mundo desde javascript') => str.split(' ').join('-')
+	const ej03 = (arr = [1,2,3,4,5,6,7,8,9,10]) => arr.filter(el => el % 2 === 0)
+	const ej04 = (pets = ['cat','dog','elephant','hawk','snake','cat']) => pets.filter(pet => pet !== 'cat')
+	const ej05 = (products = []) => {
+		const productos = products.length > 0 || [
+	  	{id: 2, nombre: 'Remera muy fachera', precio: 250},
+	  	{id: 5, nombre: 'Pantalon muy fachero', precio: 400},
+	    {id: 6, nombre: 'Camperon invierno', precio: 699},
+	    {id: 34, nombre: 'Guantes negros', precio: 199}
+		]
+		return productos.map(obj => obj.nombre)
+	}
+	const ej06 = (data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]) => data.reduce((acc, cur) => acc.concat(cur), [])
+
+	document.addEventListener('click', e => {
+		const { target: { tagName, id } } = e
+		if (tagName === 'BUTTON') {
+			switch (id) {
+				case 'ej01':
+					return alert(ej01())
+				case 'ej02':
+					return alert(ej02())
+				case 'ej03':
+					return alert(ej03())
+				case 'ej04':
+					return alert(ej04())
+				case 'ej05':
+					return alert(ej05())
+				case 'ej06':
+					return alert(ej06())
+				default:
+					console.log('ELEGIR EJERCICIO')
+			}
+		}
+	})
+
+})
